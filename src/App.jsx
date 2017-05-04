672bcd690827bcc9db3701378e8cc34f9bd67e2a
Amber Tsosie <atxambra@gmail.com>
2017-05-04 01:12:30 +0000

    Separate html into components and set initial state for App.jsx.

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,33 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+
+import MessageList from './MessageList.jsx'
+import Message from './Message.jsx'
+import ChatBar from './ChatBar.jsx'
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentUser: {name: "Bob"}, // optional. if currentUser is not defined, it means the user is Anonymous
+      messages: [
+        {
+          username: "Bob",
+          content: "Has anyone seen my marbles?",
+        },
+        {
+          username: "Anonymous",
+          content: "No, I think you lost them. You lost your marbles Bob. You lost them for good."
+        }
+      ]
+    }
+  }
+
   render() {
     return (
       <div>
-      <nav className="navbar">
-        <a href="/" className="navbar-brand">Chatty</a>
-      </nav>
-      <main className="messages">
-        <div className="message">
-          <span className="message-username">Anonymous1</span>
-          <span className="message-content">I won't be impressed with technology until I can download food.</span>
-        </div>
-        <div className="message system">
-          Anonymous1 changed their name to nomnom.
-        </div>
-      </main>
-      <footer className="chatbar">
-        <input className="chatbar-username" placeholder="Your Name (Optional)" />
-        <input className="chatbar-message" placeholder="Type a message and hit ENTER" />
-      </footer>
+        <MessageList />
+        <Message />
+        <ChatBar />
       </div>
     );
   }
---
+++ src/tmp.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class /* Name */ extends Component {
+  render() {
+    return (
+      //add html
+    )
+  }
+}
+
+export default /* Name */;
