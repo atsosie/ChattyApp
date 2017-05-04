81a6dec28ff0d6afd14837d1b26420f8df93eac6
Amber Tsosie <atxambra@gmail.com>
2017-05-04 21:24:17 +0000

    Setup and connect to WebSocket server. Client can send data to server, but incoming data not yet displaying on front-end.

--- src/App.jsx
+++ src/App.jsx
@@ -8,39 +8,50 @@ class App extends Component {
     super(props);
 
     this.state = {
-      currentUser: {name: "Bob"}, // optional. if currentUser is not defined, it means the user is Anonymous
-      messages: [
-        {
-          id: 1,
-          username: "Bob",
-          content: "Has anyone seen my marbles?"
-        },
-        {
-          id: 2,
-          username: "Anonymous",
-          content: "No, I think you lost them. You lost your marbles Bob. You lost them for good."
-        }
-      ]
+      currentUser: {name: ''}, // optional. if currentUser is not defined, it means the user is Anonymous
+      messages: [],
+      value: ''
     };
 
     this.addNewMessage = this.addNewMessage.bind(this);
   }
 
-  // componentDidMount() {
-
-  // }
 
   addNewMessage(event) {
-    console.log(event);
+    console.log('Adding new messge ...');
+
     const newMessage = {
-      id: this.state.messages.length + 1,
       username: event.username,
       content: event.content
     }
 
+    console.log('newMessage = ', newMessage);
+
     const message = this.state.messages.concat(newMessage);
-    this.setState({ messages: message })
+
+    this.socket.send(JSON.stringify(newMessage));
+
   }
+
+
+  componentDidMount() {
+    console.log('componentDidMount <App />');
+
+    this.socket = new WebSocket('ws://localhost:3001');
+
+    this.socket.onopen = function(event) {
+      console.log('Connected to server', event);
+    }.bind(this);
+
+     this.socket.onmessage = function(event) {
+        const messageFromServer = JSON.parse(event.data);
+        const newMessages = this.state.messages.concat(messageFromServer);
+        console.log('messageFromServer = ', messageFromServer);
+        console.log('messages = ', messages);
+        this.setState({ messages: newMessages })
+      }
+  }
+
 
   render() {
     return (
