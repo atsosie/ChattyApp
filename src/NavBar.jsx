21aad4fff356f2dd6338e05fbc8cd0ea2042da80
Amber Tsosie <atxambra@gmail.com>
2017-05-07 22:32:53 +0000

    Contents of navbar moved to new component.

---
+++ src/NavBar.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class NavBar extends Component {
+
+  render() {
+    return (
+      <nav className="navbar">
+        <a href="/" className="navbar-brand">Chatty</a>
+        <span className="userCount">{ this.props.userCount } users online</span>
+      </nav>
+    );
+  }
+}
+
+export default NavBar;
