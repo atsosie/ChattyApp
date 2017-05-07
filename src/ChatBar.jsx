179010fe17acd5cd7f27c66bd0397c7880cb0fce
Amber Tsosie <atxambra@gmail.com>
2017-05-07 17:54:19 +0000

    Edits to ChatBar: username defaults to 'Anonymous' if user submits form with that field blank.

--- src/ChatBar.jsx
+++ src/ChatBar.jsx
@@ -5,12 +5,12 @@ class ChatBar extends Component {
     super(props);
 
     this.state = {
-      username: this.props.username,
+      username: '',
       content: ''
     };
 
-    this.onEnterUsername = this.onEnterUsername.bind(this);
-    this.onSubmitUsername = this.onSubmitUsername.bind(this);
+    this.handleChangeUsername = this.handleChangeUsername.bind(this);
+    this.handleSubmitUsername = this.handleSubmitUsername.bind(this);
 
     this.onEnterMessage = this.onEnterMessage.bind(this);
     this.onSubmitMessage = this.onSubmitMessage.bind(this);
@@ -18,25 +18,29 @@ class ChatBar extends Component {
 
   // ----- event handlers for changing username -----
 
-  onEnterUsername(event) {
+  handleChangeUsername(event) {
     this.setState({
       username: event.target.value
     });
   }
 
-  onSubmitUsername(event) {
-  // TODO: Add flash messages for better user experience.
-  // Currently, submitting a blank form does nothing on front-end.
-    if (event.key == 'Enter' && this.state.username !== '') {
+  handleSubmitUsername(event) {
+    if (event.key == 'Enter') {
+      console.log('Enter key was pressed');
+      console.log('A name was changed: ' + this.state.username);
+
       let notification = {
         type: 'postNotification',
         username: this.state.username,
-        content: this.state.content
+        content: `${this.props.username} has changed their name to ${this.state.username}`
       }
-      this.props.addNewNotification(notification);
-    } else {
-      event.preventDefault();
-      console.log('Error: User tried to update username with a blank form. Notification not sent.');
+
+      if (!this.state.username) {
+        event.preventDefault();
+        console.log('Shhh: user is going ghost protocol')
+      } else {
+        this.props.addNewNotification(notification);
+      }
     }
   }
 
@@ -52,22 +56,27 @@ class ChatBar extends Component {
   onSubmitMessage(event) {
   // TODO: Add flash messages for better user experience.
   // Currently, submitting a blank form does nothing on front-end.
-    if (event.key == 'Enter' && this.state.content !== '') {
+    if (event.key == 'Enter') {
       console.log('Enter key was pressed');
-      let formInput = {
-        type: 'postMessage',
-        username: this.state.username,
-        content: this.state.content
-      }
-      this.props.addNewMessage(formInput);
 
-      // Reset message form field to empty after message is submitted.
-      this.setState({
-        content: ''
-      })
-    } else {
-      event.preventDefault();
-      console.log('Error: User tried to submit message with a blank form. Message not sent.');
+      if (!this.state.content) {
+        event.preventDefault();
+        console.log('Error: User tried to submit message with a blank form. Message not sent.');
+      } else {
+        let newMessge = {
+          type: 'postMessage',
+          username: this.state.username,
+          content: this.state.content
+        }
+        if (!this.state.username) {
+          newMessge.username = 'Anonymous';
+        }
+        this.props.addNewMessage(newMessge);
+        // Reset message form field to empty after message is submitted.
+        this.setState({
+          content: ''
+        })
+      }
     }
   }
 
@@ -76,18 +85,20 @@ class ChatBar extends Component {
     return (
       <footer className="chatbar">
         <input
-          onKeyUp={ this.onSubmitUsername }
-          onChange={ this.onEnterUsername }
-          value={ this.state.username }
           className="chatbar-username"
+          onChange={ this.handleChangeUsername }
+          onKeyUp={ this.handleSubmitUsername }
           placeholder="Your Name (Optional)"
+          value={ this.state.username }
+
+
         />
         <input
-          onKeyUp={ this.onSubmitMessage }
-          onChange={ this.onEnterMessage }
-          value={ this.state.content }
           className="chatbar-message"
+          onChange={ this.onEnterMessage }
+          onKeyUp={ this.onSubmitMessage }
           placeholder="Type a message and hit ENTER"
+          value={ this.state.content }
         />
       </footer>
     )
